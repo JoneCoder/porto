422c6f617088789f98bfefca40184b3efb445dc4
JoneCoder <mohummadshoriful@gmail.com>
2019-05-15 00:37:26 +0600

    User role, name, photo created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->boolean('role')->default(false);
             $table->string('username');
+            $table->string('name')->nullable();
+            $table->string('avatar')->default('default.jpg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
